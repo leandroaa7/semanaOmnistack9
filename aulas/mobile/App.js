d9cfa682fb59fbc3001f3fef9b28cda652d7e324
leandroaa7 <leandroaalexandre@gmail.com>
2020-04-03 01:52:52 -0300

    aula4/2

--- aulas/mobile/App.js
+++ aulas/mobile/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes'
+
+/** Toda a estilização é feita com flexbox */
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>
-        Hello Omnistack
-        </Text>
-
-    </View>
+    <Routes />
   );
 }
 
-/** Toda a estilização é feita com flexbox */
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-});
--- aulas/mobile/src/pages/List.js
+++ aulas/mobile/src/pages/List.js
@@ -1,6 +1,43 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    Image,
+    SafeAreaView, // View que não ultrapassa a status bar ou os cantos do começo da tela
+    StyleSheet, //classe para criar os styles dos elementos
+    AsyncStorage // similar ao localstorage
+} from 'react-native';
+
+import logo from '../assets/logo.png';
+
 
 export default function List() {
-    return <View />
+    const [techs, setTechs] = useState([]);
+
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs
+                .split(',')
+                .map(tech => tech.trim());
+            setTechs(techsArray)
+        })
+    }, [])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo: {
+        height: 32,
+        resizeMode: "contain", //faz com que o conteúdo da imagem fique contido no tamanho setado
+        alignSelf: "center", // alinhar no centro
+        marginTop: 10
+    }
+})
