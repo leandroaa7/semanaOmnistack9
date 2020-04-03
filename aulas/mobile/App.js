61c0755ef19319b896a1c77ab05e713108994524
leandroaa7 <leandroaalexandre@gmail.com>
2020-04-03 00:28:31 -0300

    aula4 iniciando mobile

---
+++ aulas/mobile/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Hello Omnistack
+        </Text>
+
+    </View>
+  );
+}
+
+/** Toda a estilização é feita com flexbox */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+});
